a688402f669bce0905ebfa9264a120c8487f0501
Abtin Bateni <abtinbateni@gmail.com>
2019-11-16 11:05:45 -0800

    Revert "Minor fix"
    This reverts commit 5c85cabbf07e6e20c504b424ab853f4c3f674797.

--- dashboard-simple/src/App.js
+++ dashboard-simple/src/App.js
@@ -1,32 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Dashboard from './dashboard';
-import {BrowserRouter as Router, Route, Switch,} from "react-router-dom";
-import {NotificationContainer, NotificationManager} from "react-notifications";
-import Users from "./Users";
-
-export const createNotification = ({data}) => {
-    const {message} = data;
-    const status = data.status || (data.success ? 'success' : 'error');
-    console.log('notift: ', data);
-    console.log('st: ', status);
-
-    switch (status) {
-        case 'info':
-            NotificationManager.info(message);
-            break;
-        case 'success':
-            NotificationManager.success(message);
-            break;
-        case 'warning':
-            NotificationManager.warning(message, '', 3000);
-            break;
-        case 'error':
-            NotificationManager.error(message, 'Click me!', 5000, () => {
-                alert('callback');
-            });
-            break;
-    }
-};
 
 class App extends Component {
   render() {
@@ -38,18 +11,8 @@ class App extends Component {
           {/*</h2>*/}
         {/*</div>*/}
         <div style={{ position: 'fixed', top: 0, left: 0, width: '100%'}}>
-            <Router>
-                <Switch>
-                    <Route path="/users">
-                        <Users />
-                    </Route>
-                    <Route path="/">
-                        <Dashboard />
-                    </Route>
-                </Switch>
-            </Router>
+          <Dashboard />
         </div>
-        <NotificationContainer />
       </div>
     );
   }
--- dashboard-simple/src/dashboard.jsx
+++ dashboard-simple/src/dashboard.jsx
@@ -1,14 +1,30 @@
-import React, {Component} from 'react';
-import {withStyles} from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import axios from 'axios';
+import deepPurple from '@material-ui/core/colors/deepPurple';
 import 'react-notifications/lib/notifications.css';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+import NewEvent from './NewEvent';
 import Chat from './Chat';
 import Card from './Card';
-import {baseUrl} from './config';
-import NavBar from "./NavBar";
-import {createNotification} from "./App";
+import { baseUrl } from './config';
 
 const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+    float: 'right',
+  },
+  input: {
+    display: 'none',
+  },
+  customColor: {
+    color: theme.palette.getContrastText(deepPurple[ 400 ]),
+    backgroundColor: deepPurple[ 400 ],
+    '&:hover': {
+      backgroundColor: deepPurple[ 600 ],
+    },
+  }
 });
 
 class Dashboard extends Component {
@@ -16,12 +32,35 @@ class Dashboard extends Component {
     super(props);
     this.state = {
       pollItemId: null,
+      dialogOpen: false,
       chatOpen: false,
       events: [],
       comments: [],
     };
   }
+  createNotification = ({data}) => {
+    const {message} = data;
+    const status = data.status || (data.success ? 'success': 'error');
+    console.log('notift: ', data);
+    console.log('st: ', status);
 
+    switch (status) {
+      case 'info':
+        NotificationManager.info(message);
+        break;
+      case 'success':
+        NotificationManager.success(message);
+        break;
+      case 'warning':
+        NotificationManager.warning(message, '', 3000);
+        break;
+      case 'error':
+        NotificationManager.error(message, 'Click me!', 5000, () => {
+          alert('callback');
+        });
+        break;
+    }
+  };
   getEvents = () => {
     axios.get(`${baseUrl}/event`, {
       headers: {
@@ -35,7 +74,23 @@ class Dashboard extends Component {
         }))});
     });
   };
-
+  normalizeEvent = ({participants, pollItems, ...rest}) => ({
+    pollTimes: pollItems.map(({beginsAt, until}) => [beginsAt, until]),
+    participantEmails: participants && participants.split(','),
+    ...rest,
+  });
+  createEvent = event => {
+    this.closeDialog();
+    console.log(event);
+    const finalEvent = this.normalizeEvent(event);
+    axios.post(`${baseUrl}/event`, {
+      ...finalEvent,
+    }, {
+      headers: {
+        Authorization: 'Bearer ' + document.cookie.substring(6),
+      },
+    }).then(this.createNotification).then(this.getEvents);
+  };
   setVote = ({ verdict, pollItemId }) => {
     axios.post(`${baseUrl}/poll/vote`, {
       verdict,
@@ -44,9 +99,8 @@ class Dashboard extends Component {
       headers: {
         Authorization: 'Bearer ' + document.cookie.substring(6),
       },
-    }).then(createNotification).then(this.getEvents);
+    }).then(this.createNotification).then(this.getEvents);
   };
-
   closeEvent = ({eventId, select: pollItemId}) => {
     axios.put(`${baseUrl}/event/close`, {
       eventId, pollItemId,
@@ -54,9 +108,8 @@ class Dashboard extends Component {
       headers: {
         Authorization: 'Bearer ' + document.cookie.substring(6),
       },
-    }).then(createNotification).then(this.getEvents);
+    }).then(this.createNotification).then(this.getEvents);
   };
-
   openEvent = ({eventId}) => {
     axios.put(`${baseUrl}/event/open`, {
       eventId,
@@ -64,13 +117,11 @@ class Dashboard extends Component {
       headers: {
         Authorization: 'Bearer ' + document.cookie.substring(6),
       },
-    }).then(createNotification).then(this.getEvents);
+    }).then(this.createNotification).then(this.getEvents);
   };
-
   openChat = (pollItemId) => {
     this.setState({pollItemId, chatOpen: true}, this.getComments);
   };
-
   handleSendComment = (text) => {
     const {pollItemId} = this.state;
     axios.post(`${baseUrl}/poll_item/comment`, {
@@ -80,9 +131,8 @@ class Dashboard extends Component {
       headers: {
         Authorization: 'Bearer ' + document.cookie.substring(6),
       },
-    }).then(createNotification).then(this.getComments);
+    }).then(this.createNotification).then(this.getComments);
   };
-
   getComments = () => {
     const {pollItemId} = this.state;
     axios.post(`${baseUrl}/poll_item/comment/fetch`, {
@@ -98,14 +148,23 @@ class Dashboard extends Component {
     this.getEvents();
   }
   closeDialog = () => {
-    this.setState({ chatOpen: false });
+    this.setState({ dialogOpen: false, chatOpen: false });
   };
   render() {
-    const { chatOpen, events } = this.state;
+    const { classes } = this.props;
+    const { dialogOpen, chatOpen, events } = this.state;
 
     return (
       <div>
-        <NavBar onUpdate={this.getEvents} />
+        <div style={{marginRight: 50}} >
+          <Button variant="contained" color="primary" className={[classes.button, classes.customColor]} onClick={() => this.setState({ dialogOpen: true })}>
+            Create
+          </Button>
+          <Button variant="contained" color="primary" className={[classes.button, classes.customColor]}>
+            Logout
+          </Button>
+        </div>
+        <NewEvent open={dialogOpen} handleClose={this.closeDialog} createEvent={this.createEvent} />
         <Chat open={chatOpen} handleClose={this.closeDialog} onSend={this.handleSendComment} comments={this.state.comments} />
         <div style={{ paddingTop: 100, marginRight: 100, marginLeft: 100, marginBottom: 100, height: window.innerHeight }} >
           <div style={{ width: '100%', height: '100%', overflow: 'scroll' }}>
@@ -127,6 +186,7 @@ class Dashboard extends Component {
             )}
           </div>
         </div>
+        <NotificationContainer />
       </div>
     );
   }
